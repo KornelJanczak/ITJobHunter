958593047834fbb1e785d3286a8558472c3fd79c
KornelJanczak <korneljanczak10@gmail.com>
2024-09-07 19:28:21 +0200

    feat(api): create controller

--- apps/server/src/modules/jobScraper/controller/index.ts
+++ apps/server/src/modules/jobScraper/controller/index.ts
@@ -1,3 +1,13 @@
-export const jobScraperController = async (req: Request, res: Response) => {
-    return "Hello World";
+import { NextFunction, Request, Response } from "express";
+
+export const jobScraperController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    return res.status(200).json({ message: "Job Scraper Controller" });
+  } catch (err) {
+    next(err);
+  }
 };
