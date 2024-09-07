f3467ca71ed9cd17b218c81d6a96f80c98e3c7e3
KornelJanczak <korneljanczak10@gmail.com>
2024-09-07 19:28:01 +0200

    feat(interface): add JobQuery interface

--- packages/interfaces/src/job.ts
+++ packages/interfaces/src/job.ts
@@ -1,3 +1,16 @@
 export interface JobQuery {
-  id: string;
+  query: string;
+  category?:
+    | "frontend"
+    | "backend"
+    | "ui/ux"
+    | "machineLearning"
+    | "cyberSecurity";
+  location?: string;
+  minimumSalary?: number;
+  maximumSalary?: number;
+  jobType?: "fullTime" | "partTime" | "contract" | "remote";
+  lastUpdated?: number;
+  typeOfWorkplace?: "onSite" | "hybrid" | "remote";
+  positionLevel?: "junior" | "senior" | "leader" | "manager";
 }
