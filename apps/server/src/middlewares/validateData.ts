d44cfed31b9b1da23b7003d0e95b520efdb1a824
KornelJanczak <korneljanczak10@gmail.com>
2024-09-07 19:29:18 +0200

    fix(shared packages): error related with types for shared packages

--- apps/server/src/middlewares/validateData.ts
+++ apps/server/src/middlewares/validateData.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { z, ZodError } from "zod";
 import { StatusCodes } from "http-status-codes";
 import BadRequestError from "../errors/badRequestError";
-import { JobQuery } from "../../../utils/interfaces/job";
+import { JobQuery } from "@repo/interfaces/job";
 
 export const validateData = (schema: z.ZodObject<any, any>) => {
   return (req: Request, res: Response, next: NextFunction) => {
