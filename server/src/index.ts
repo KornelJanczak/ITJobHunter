dfc18e2394439f9f3d46f03c928571c6461137be
Kornel Janczak <82079502+KornelJanczak@users.noreply.github.com>
2024-09-07 14:29:16 +0200

    refactor(deprecated): delete old server

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "express";
-import createApp from "./app";
-
-const PORT = process.env.PORT || 3000;
-const app: Application = createApp();
-
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
-});
--- server/src/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-import crypto from "crypto";
-
-export function generateSessionKey(): string {
-  return crypto.randomBytes(32).toString("hex");
-}
