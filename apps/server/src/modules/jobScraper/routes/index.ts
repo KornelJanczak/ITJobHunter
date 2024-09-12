637761df491606b7f8d72185c5102c71b3c5566b
KornelJanczak <korneljanczak10@gmail.com>
2024-09-12 21:27:37 +0200

    refactor(router): change schema

--- apps/server/src/modules/jobScraper/routes/index.ts
+++ apps/server/src/modules/jobScraper/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { jobScraperController } from "../controller";
 import { validateData } from "../../../middlewares/validateData";
-import { jobQuerySchema } from "@repo/schemas/jobSchemas";
+import { jobQuerySchemaWithValidation } from "../middlewares/jobQuerySchemaWithValidation";
 
 export default (router: Router) => {
   router.get(
     "/job-scraper",
-    validateData(jobQuerySchema),
+    validateData(jobQuerySchemaWithValidation),
     jobScraperController
   );
 };
