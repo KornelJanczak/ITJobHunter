c198dd7297cbed272e009875c639b67dc9d61331
KornelJanczak <korneljanczak10@gmail.com>
2024-09-12 21:27:24 +0200

    feat(service): during coding

--- apps/server/src/modules/jobScraper/services/scrapeJustJoinIT.ts
+++ apps/server/src/modules/jobScraper/services/scrapeJustJoinIT.ts
@@ -7,15 +7,46 @@ export const scrapeJustJoinIT = async ({
   jobQuery,
 }: ScrapeOptions) => {
   try {
+    console.log(jobQuery, "JobQuery");
+    let jobs;
+    const { content, techStack, location } = jobQuery;
     const page = await browser.newPage();
-    await page.goto(url);
-
-    console.log("site");
+    let path = `${url}/${location ? location : "all-locations"}`;
+    await page.goto(path);
 
     await page.waitForSelector("#cookiescript_injected");
     await page.click("#cookiescript_accept");
     await page.waitForSelector('input[placeholder="Search"]');
-    await page.type('input[placeholder="Search"]', jobQuery.content);
-    await page.keyboard.press("Enter");
+
+    if (content) {
+      await page.type('input[placeholder="Search"]', content);
+      await page.keyboard.press("Enter");
+    }
+
+    await page.waitForSelector(".offer_list_offer_link");
+
+    if (techStack) {
+      console.log("techStack", techStack);
+
+      const jobLinks = await page.evaluate(() => {
+        console.log("EVALUATE");
+
+        const links = Array.from(
+          document.querySelectorAll(".offer_list_offer_link")
+        );
+
+        console.log(jobLinks, "LINKS");
+        return links;
+      });
+
+      console.log("jobLinks", jobLinks);
+
+      const maxConcurrentPages = 5;
+      const jobDataPromises = [];
+
+      for (let i = 0; i < jobLinks.length; i += maxConcurrentPages) {
+        console.log(jobLinks[i], "LINK");
+      }
+    }
   } catch {}
 };
