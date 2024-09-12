7d8aced03c3f9f039abe081d2c9266eabe53b274
KornelJanczak <korneljanczak10@gmail.com>
2024-09-12 21:27:58 +0200

    refactor(types): change job query type

--- packages/interfaces/src/job.ts
+++ packages/interfaces/src/job.ts
@@ -1,32 +1,4 @@
-type TechStack =
-  | "react"
-  | "node"
-  | "html"
-  | "php"
-  | "python"
-  | "java"
-  | "c#"
-  | "ts"
-  | "js"
-  | "c"
-  | "c++"
-  | "ruby"
-  | ".net";
+import z from "zod";
+import { jobQuerySchema } from "@repo/schemas/jobSchemas";
 
-export interface JobQuery {
-  content: string;
-  category?:
-    | "frontend"
-    | "backend"
-    | "ui/ux"
-    | "machineLearning"
-    | "cyberSecurity";
-  location?: string;
-  minimumSalary?: number;
-  maximumSalary?: number;
-  jobType?: "fullTime" | "partTime" | "contract" | "remote";
-  lastUpdated?: number;
-  typeOfWorkplace?: "onSite" | "hybrid" | "remote";
-  positionLevel?: "junior" | "senior" | "leader" | "manager";
-  techStack?: TechStack[];
-}
+export type JobQuery = z.infer<typeof jobQuerySchema>;
