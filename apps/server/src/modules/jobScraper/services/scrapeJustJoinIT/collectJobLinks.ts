3795bb03f8b8a122782c9389eb17fe709ea69e7c
KornelJanczak <korneljanczak10@gmail.com>
2024-09-24 19:43:23 +0200

    refactor(service): remove unecessary lines of code

--- apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/collectJobLinks.ts
+++ apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/collectJobLinks.ts
@@ -1,26 +1,18 @@
-import { Page } from "puppeteer";
+import { type Page } from "puppeteer";
 
 export const collectJobLinks = async (page: Page) => {
-  // Przesuń mysz na współrzędne (100, 100)
-
-
-  // console.log("Waiting for elements to be visible...");
-  // await page.waitForSelector(".MuiBox-root.css-8xzgzu", {
-  //   visible: true,
-  //   timeout: 10000,
+  // await page.evaluate(() => {
+  //   window.scrollBy(0, window.innerHeight * 10);
   // });
 
-  const elements = await page.$$(".MuiBox-root.css-8xzgzu");
+  const elements = await page.$$("a[target='_parent']");
 
   console.log(elements, "elements");
   const jobLinks = [];
 
   for (const element of elements) {
-    const linkHandle = await element.$("a");
-    if (linkHandle) {
-      const href = await page.evaluate((el) => el.href, linkHandle);
-      jobLinks.push(href);
-    }
+    const href = await page.evaluate((el) => el.href, element);
+    jobLinks.push(href);
   }
 
   console.log(jobLinks, "jobLinks");
