55bc5a1b4abb199cee1fcdfcea2a7d5386f967e5
KornelJanczak <korneljanczak10@gmail.com>
2024-10-01 19:32:30 +0200

    feat(service): extend ScrapeJustJoinIT service

--- apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/index.ts
+++ apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/index.ts
@@ -1,5 +1,5 @@
 import { Browser } from "puppeteer";
-import { ScrapeOptions } from "../../interfaces";
+import { JustJoinITOffer, ScrapeOptions } from "../../interfaces";
 import e, { NextFunction } from "express";
 import { Page } from "puppeteer";
 import BadRequestError from "../../../../errors/badRequestError";
@@ -22,9 +22,7 @@ export default class ScrapeJustJoinIT {
 
       await this.openPage(page, path, next);
       await this.searchJobOffers({ page, jobQuery, path, next });
-      const jobs = await this.scrollAndCollectData(page, 100);
-
-      console.log(jobs);
+      const jobs = await this.scrollAndCollectData(page);
       await this.browser.close();
 
       return jobs;
@@ -64,7 +62,7 @@ export default class ScrapeJustJoinIT {
     path,
     next,
   }: SearchJobOffers): Promise<void> {
-    const { content, techStack } = jobQuery;
+    const { content, techStack, positionLevel } = jobQuery;
 
     if (techStack) {
       console.log("TECH STACK", techStack);
@@ -74,6 +72,8 @@ export default class ScrapeJustJoinIT {
 
     if (content) path += `?keyword=${content}`;
 
+    if (positionLevel) path += `/experience-level_${positionLevel}`;
+
     try {
       await page.goto(path);
     } catch (err) {
@@ -82,119 +82,64 @@ export default class ScrapeJustJoinIT {
           code: 400,
           logging: true,
           message: "Failed to search for job offers by job query content",
+          context: { err },
         })
       );
     }
   }
 
-  private async collectData(page: Page) {
+  private async collectData(page: Page): Promise<JustJoinITOffer[]> {
     const elements = await page.$$(".MuiBox-root.css-ai36e1");
 
-    const jobOffers = [];
+    const jobOffers: JustJoinITOffer[] = [];
 
     for (const element of elements) {
-      const title = await page.evaluate(
-        (el) => el.querySelector("h3")?.textContent || "",
-        element
-      );
+      try {
+        const [title, location, url] = await Promise.all([
+          element.$eval("h3", (el) => el.textContent || ""),
+          element.$eval(".css-1o4wo1x", (el) => el.textContent || ""),
+          element.$eval("a", (el) => el.getAttribute("href") || ""),
+        ]);
 
-      const location = await page.evaluate(
-        (el) => el.querySelector(".css-1o4wo1x")?.textContent || "",
-        element
-      );
-
-      const url = await page.evaluate(
-        (el) => el.querySelector("a")?.href || "",
-        element
-      );
-
-      jobOffers.push({ title, location, url });
+        jobOffers.push({ title, location, url });
+      } catch (err) {
+        throw new BadRequestError({
+          code: 400,
+          message: "Failed to collect data",
+          logging: true,
+          context: { err },
+        });
+      }
     }
 
     return jobOffers;
   }
 
-  private async scrollAndCollectData(
-    page: Page,
-    itemTargetCount: number
-  ): Promise<{}[]> {
+  private async scrollAndCollectData(page: Page): Promise<{}[]> {
     let allJobs: {}[] = [];
-    let previousHeight: number;
+    let previousHeight = 0;
+    let currentHeight = 0;
 
-    while (itemTargetCount > allJobs.length) {
-      // const jobs = await this.collectData(page);
-      allJobs = await this.collectData(page);
+    while (true) {
+      const data = await this.collectData(page);
+      allJobs = [...new Set([...data, ...allJobs])];
+      await this.waitFor(3000);
 
-      // await autoScroll(page);
+      await autoScroll(page);
 
-      previousHeight = await page.evaluate(() => document.body.scrollHeight);
-      await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-      await page.waitForFunction(
-        () => document.body.scrollHeight > previousHeight
-      );
+      currentHeight = await page.evaluate(() => document.body.scrollHeight);
+      const endOfPage = await page.$("footer");
 
-      
+      if (endOfPage) break;
+      if (currentHeight === previousHeight) break;
 
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      // const currentHeight = await page.evaluate(
-      //   () => document.body.scrollHeight
-      // );
-
-      // if (currentHeight === previousHeight) {
-      //   console.log("currentHeight", currentHeight);
-      //   console.log("previousHeight", previousHeight);
-
-      //   break;
-      // }
-      // previousHeight = currentHeight;
+      previousHeight = currentHeight;
     }
 
     return allJobs;
   }
 
-  // private async collectJobsData(page: Page): Promise<string[]> {
-  //   const elements = await page.$$("a[target='_parent']");
-
-  //   const jobLinks = [];
-
-  //   for (const element of elements) {
-  //     const href = await page.evaluate((el) => el.href, element);
-  //     jobLinks.push(href);
-  //   }
-
-  //   return jobLinks;
-  // }
-  // private async collectJobOffersDetails(cluster: Cluster, jobLinks: string[]) {
-  //   await cluster.task(async ({ page, data: jobLink }) => {
-  //     await page.goto(jobLink);
-  //     let isBtnDisabled = false;
-
-  //     while (!isBtnDisabled) {
-  //       await page.waitForSelector("h1");
-  //     }
-  //   });
-
-  //   await cluster.queue(jobLinks);
-  // }
-
-  // private async initCluster(): Promise<Cluster> {
-  //   try {
-  //     return await Cluster.launch({
-  //       concurrency: Cluster.CONCURRENCY_PAGE,
-  //       maxConcurrency: 100,
-  //       monitor: true,
-  //       puppeteerOptions: {
-  //         headless: false,
-  //         userDataDir: "./tmp",
-  //       },
-  //     });
-  //   } catch (err) {
-  //     throw new BadRequestError({
-  //       message: "Failed to initialize cluster",
-  //       logging: true,
-  //       code: 400,
-  //       context: { error: err },
-  //     });
-  //   }
-  // }
+  private waitFor(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
 }
