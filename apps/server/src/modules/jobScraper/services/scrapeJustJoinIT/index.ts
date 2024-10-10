4fc3816a658633fe91573a9ebffd3541101a3a86
KornelJanczak <korneljanczak10@gmail.com>
2024-10-10 20:55:42 +0200

    refactor: change functions structure

--- apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/index.ts
+++ apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/index.ts
@@ -1,55 +1,51 @@
-import { Browser } from "puppeteer";
-import { JustJoinITOffer, ScrapeOptions } from "../../interfaces";
-import BadRequestError from "../../../../errors/badRequestError";
-import { type IDataCollector } from "./dataCollector";
-import { type IJobSearcher } from "./jobSearcher";
-import { type IPageOpener } from "./pageOpener";
+import { type Page } from "puppeteer";
+import { type IDataCollector } from "../../interfaces";
+import { type IJobSearcher } from "../../interfaces";
+import { type IPageOpener } from "../../interfaces";
+import {
+  IJobScraperService,
+  IScraperServiceDependencies,
+  JustJoinITOffer,
+  ScrapeOptions,
+} from "../../interfaces";
+import CoreJobScraperService from "../coreJobScraperService";
+import { pageOpener } from "./pageOpener";
+import { dataCollector } from "./dataCollector";
+import { jobSearcher } from "./jobSearcher";
 
-interface JustJoinITScraper {
-  scrape(options: ScrapeOptions): Promise<JustJoinITOffer[]>;
-}
+class ScrapeJustJoinIT<T extends JustJoinITOffer>
+  extends CoreJobScraperService
+  implements IJobScraperService<T>
+{
+  private pageOpener: IPageOpener<T>;
+  private jobSearcher: IJobSearcher<T>;
+  private dataCollector: IDataCollector<T>;
 
-interface ScrapeJustJoinITConfig {
-  browser: Browser;
-  pageOpener: IPageOpener;
-  jobSearcher: IJobSearcher;
-  dataCollector: IDataCollector;
-}
-
-export default class ScrapeJustJoinIT implements JustJoinITScraper {
-  private browser: Browser;
-  private pageOpener: IPageOpener;
-  private jobSearcher: IJobSearcher;
-  private dataCollector: IDataCollector;
-
-  constructor(config: ScrapeJustJoinITConfig) {
-    this.browser = config.browser;
+  constructor(config: IScraperServiceDependencies<T>) {
+    super();
     this.pageOpener = config.pageOpener;
     this.jobSearcher = config.jobSearcher;
     this.dataCollector = config.dataCollector;
   }
 
-  async scrape({ url, jobQuery, next }: ScrapeOptions) {
-    try {
-      const page = await this.browser.newPage();
-      await this.pageOpener.openPage(page, url, next);
-      await this.jobSearcher.searchJobOffers({
-        page,
-        jobQuery,
-        path: url,
-        next,
-      });
+  protected async executeScrape(
+    page: Page,
+    { url, jobQuery, next }: ScrapeOptions
+  ): Promise<T[]> {
+    await this.pageOpener.openPage(page, url, next);
+    await this.jobSearcher.searchJobOffers({
+      page,
+      jobQuery,
+      path: url,
+      next,
+    });
 
-      return await this.dataCollector.scrollAndCollectData(page);
-    } catch (err) {
-      throw new BadRequestError({
-        message: "Failed to scrape JustJoinIT",
-        logging: true,
-        code: 400,
-        context: { error: err },
-      });
-    } finally {
-      await this.browser.close();
-    }
+    return await this.dataCollector.scrollAndCollectData(page);
   }
 }
+
+export const scrapeJustJoinIT = new ScrapeJustJoinIT({
+  pageOpener,
+  jobSearcher,
+  dataCollector,
+});
