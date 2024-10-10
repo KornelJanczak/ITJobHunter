1df1000b6b417297118b9348ddec7156571c5f6e
KornelJanczak <korneljanczak10@gmail.com>
2024-10-10 20:53:12 +0200

    refactor: move service interfaces into separate file

--- apps/server/src/modules/jobScraper/interfaces/index.ts
+++ apps/server/src/modules/jobScraper/interfaces/index.ts
@@ -1,4 +1,3 @@
-import { Browser } from "puppeteer";
 import { JobQuery } from "@repo/interfaces/job";
 import { NextFunction } from "express";
 import { Page } from "puppeteer";
@@ -20,4 +19,32 @@ export interface JustJoinITOffer {
   title: string;
   location: string;
   url: string;
+}
+
+export interface ICoreJobScraperService {
+  initBrowser(): Promise<void>;
+  closeBrowser(): Promise<void>;
+}
+
+export interface IJobScraperService<T> {
+  scrape(options: ScrapeOptions): Promise<T[]>;
+}
+
+export interface IDataCollector<T> {
+  collectData(page: Page): Promise<T[]>;
+  scrollAndCollectData(page: Page): Promise<T[]>;
+}
+
+export interface IJobSearcher<T> {
+  searchJobOffers(options: SearchJobOffers): Promise<void>;
+}
+
+export interface IPageOpener<T> {
+  openPage(page: Page, path: string, next: NextFunction): Promise<void>;
+}
+
+export interface IScraperServiceDependencies<T> {
+  pageOpener: IPageOpener<T>;
+  jobSearcher: IJobSearcher<T>;
+  dataCollector: IDataCollector<T>;
 }
