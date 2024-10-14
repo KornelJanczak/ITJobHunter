8f6a3d9b22de2377d0b11aefc42dec27fe554a80
KornelJanczak <korneljanczak10@gmail.com>
2024-10-14 19:59:45 +0200

    refactor(service): refactor NoFluffJobsScraperService in scrapeNoFluffJobs

--- apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/index.ts
+++ apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/index.ts
@@ -1,36 +1,45 @@
 import { type Page } from "puppeteer";
 import { type IDataCollector } from "../../interfaces";
 import { type IJobSearcher } from "../../interfaces";
-import { type IPageOpener } from "../../interfaces";
-import CoreJobScraperService from "../coreJobScraperService";
 import {
   IJobScraperService,
   IScraperServiceDependencies,
   JobOffer,
   ScrapeOptions,
 } from "../../interfaces";
-import { pageOpener } from "./pageOpener";
+
+import { jobSearcher } from "./jobSearcher";
+import { dataCollector } from "./dataCollector";
+import AbstractScraperService from "../abstract/abstractScraperService";
 
 class NoFluffJobsScraperService<T extends JobOffer>
-  extends CoreJobScraperService
+  extends AbstractScraperService
   implements IJobScraperService<T>
 {
-  private pageOpener: IPageOpener<T>;
   private jobSearcher: IJobSearcher<T>;
   private dataCollector: IDataCollector<T>;
 
   constructor(config: IScraperServiceDependencies<T>) {
     super();
-    this.pageOpener = config.pageOpener;
     this.jobSearcher = config.jobSearcher;
     this.dataCollector = config.dataCollector;
   }
   protected async executeScrape(
     page: Page,
-    options: ScrapeOptions
-  ): Promise<any> {}
+    { url, jobQuery, next }: ScrapeOptions
+  ): Promise<any> {
+    await this.jobSearcher.searchJobOffers({
+      page,
+      jobQuery,
+      path: url,
+      next,
+    });
+
+    return [];
+  }
 }
 
-export const noFluffJobsScraperService = new NoFluffJobsScraperService({
-  pageOpener,
+export const scrapeNoFluffJobs = new NoFluffJobsScraperService({
+  jobSearcher,
+  dataCollector,
 });
