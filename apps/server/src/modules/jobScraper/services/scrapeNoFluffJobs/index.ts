19191964cd4a0605b56a494cc80f7fdff67f5b66
KornelJanczak <korneljanczak10@gmail.com>
2024-10-14 18:59:53 +0200

    refactor(service): update NoFluffJobsScraperService class to use dependency injection

--- apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/index.ts
+++ apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/index.ts
@@ -1,19 +1,36 @@
 import { type Page } from "puppeteer";
+import { type IDataCollector } from "../../interfaces";
+import { type IJobSearcher } from "../../interfaces";
+import { type IPageOpener } from "../../interfaces";
 import CoreJobScraperService from "../coreJobScraperService";
 import {
-  type IJobScraperService,
-  type JobOffer,
-  type ScrapeOptions,
+  IJobScraperService,
+  IScraperServiceDependencies,
+  JobOffer,
+  ScrapeOptions,
 } from "../../interfaces";
+import { pageOpener } from "./pageOpener";
 
 class NoFluffJobsScraperService<T extends JobOffer>
   extends CoreJobScraperService
   implements IJobScraperService<T>
 {
+  private pageOpener: IPageOpener<T>;
+  private jobSearcher: IJobSearcher<T>;
+  private dataCollector: IDataCollector<T>;
+
+  constructor(config: IScraperServiceDependencies<T>) {
+    super();
+    this.pageOpener = config.pageOpener;
+    this.jobSearcher = config.jobSearcher;
+    this.dataCollector = config.dataCollector;
+  }
   protected async executeScrape(
     page: Page,
     options: ScrapeOptions
   ): Promise<any> {}
 }
 
-export const noFluffJobsScraperService = new NoFluffJobsScraperService();
+export const noFluffJobsScraperService = new NoFluffJobsScraperService({
+  pageOpener,
+});
