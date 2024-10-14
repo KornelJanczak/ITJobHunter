a3cd94cbf696e5fa973e6b105486265ef32d3ec1
KornelJanczak <korneljanczak10@gmail.com>
2024-10-14 18:59:21 +0200

    refactor(service): add AbstractJobSearcher class

---
+++ apps/server/src/modules/jobScraper/services/abstract/abstractJobSearcher.ts
@@ -0,0 +1,40 @@
+import { type SearchJobOffers } from "../../interfaces";
+
+export abstract class AbstractJobSearcher {
+  abstract searchJobOffers({
+    page,
+    jobQuery,
+    path,
+    next,
+  }: SearchJobOffers): Promise<void>;
+
+  protected abstract filterLocation(
+    path: string,
+    location: string | undefined
+  ): string;
+  protected abstract filterTechStack(
+    path: string,
+    techStack: string[] | undefined
+  ): string;
+
+  protected abstract filterContent(
+    path: string,
+    content: string | undefined
+  ): string;
+
+  protected abstract filterPositionLevel(
+    path: string,
+    positionLevel: string | undefined
+  ): string;
+
+  protected abstract filterSalary(
+    path: string,
+    minimumSalary: number | undefined,
+    maximumSalary: number | undefined
+  ): string;
+
+  protected abstract filterJobType(
+    path: string,
+    jobType: string[] | undefined
+  ): string;
+}
