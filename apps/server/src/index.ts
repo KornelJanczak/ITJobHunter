a6dc6c8b4ecc75ff57b4a6f29d18ff9db3e12b1f
KornelJanczak <korneljanczak10@gmail.com>
2024-10-17 21:32:04 +0200

    refactor(controller): Add root route handler

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -4,6 +4,10 @@ import createApp from "./app";
 const PORT = process.env.PORT || 3000;
 const app: Application = createApp();
 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
