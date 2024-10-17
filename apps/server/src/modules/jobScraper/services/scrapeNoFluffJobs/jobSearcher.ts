f69ece454c7f05e9ad70e665b32db9d95d24206f
KornelJanczak <korneljanczak10@gmail.com>
2024-10-17 21:31:54 +0200

    refactor(service): Update JobSearcher to include additional search criteria and filter options

--- apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/jobSearcher.ts
+++ apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/jobSearcher.ts
@@ -19,9 +19,16 @@ export class JobSearcher extends AbstractJobSearcher {
     } = jobQuery;
 
     path = this.filterLocation(path, location);
+    path = this.filterJobType(path, jobType);
+    console.log("filter job type", path);
+
     path = this.filterTechStack(path, techStack);
     path = this.filterPositionLevel(path, positionLevel);
-    path = this.filterContent(path, content);
+    path = this.filterSalary(path, minimumSalary, maximumSalary);
+    // path = this.filterContent(path, content);
+
+    console.log("AFTER job searcher", path);
+
     await page.goto(path);
   }
 
@@ -43,17 +50,11 @@ export class JobSearcher extends AbstractJobSearcher {
         console.log(techStack);
         const basePath = path.split("?")[0];
         const criteria = `criteria=requirement%3D${techStack.map(encodeURIComponent).join(",")}`;
-        path += `${basePath}?${criteria}`;
+        path = `${basePath}?${criteria}`;
         break;
     }
 
     return path;
-  }
-
-  protected filterContent(path: string, content: string | undefined): string {
-    const basePath = path.split("?")[0];
-    const criteria = `criteria=requirement%3D${content}`;
-    return `${basePath}?${criteria}`;
   }
 
   protected filterPositionLevel(
@@ -67,16 +68,27 @@ export class JobSearcher extends AbstractJobSearcher {
       case "junior":
         const seniorities = ["junior", "trainee"];
         criteria = `criteria=seniority%3D${seniorities.map(encodeURIComponent).join(",")}`;
-        path += `${basePath}?${criteria}`;
+        path = `${basePath}?${criteria}`;
+        console.log("junior", path);
+
         break;
       default:
         if (positionLevel) {
           criteria = `criteria=seniority%3D${encodeURIComponent(positionLevel)}`;
-          path += `${basePath}?${criteria}`;
+          path = `${basePath}?${criteria}`;
         }
         break;
     }
+
+    console.log("path", path);
+
     return path;
+  }
+
+  protected filterContent(path: string, content: string | undefined): string {
+    const basePath = path.split("?")[0];
+    const criteria = `criteria=requirement%3D${content}`;
+    return `${basePath}?${criteria}`;
   }
 
   protected filterSalary(
@@ -84,13 +96,28 @@ export class JobSearcher extends AbstractJobSearcher {
     minimumSalary: number | undefined,
     maximumSalary: number | undefined
   ): string {
-    // Implementation will go here
-    return path;
+    const basePath = path.split("?")[0];
+    const criteria: string[] = [];
+
+    if (minimumSalary !== undefined)
+      criteria.push(`salary%3Epln${minimumSalary}m`);
+
+    if (maximumSalary !== undefined)
+      criteria.push(`salary%3Cpln${maximumSalary}m`);
+
+    const criteriaString =
+      criteria.length > 0 ? `criteria=${criteria.join(" ")}` : "";
+    return `${basePath}?${criteriaString}`;
   }
 
   protected filterJobType(path: string, jobType: string[] | undefined): string {
-    // Implementation will go here
-    return path;
+    if (!jobType) return path;
+    if (jobType.length === 1) return (path += `/${jobType[0]}`);
+
+    const basePath = path.split("?")[0];
+    const criteria = `%3D${jobType.map(encodeURIComponent).join(",")}`;
+
+    return `${basePath}?${criteria}`;
   }
 }
 
