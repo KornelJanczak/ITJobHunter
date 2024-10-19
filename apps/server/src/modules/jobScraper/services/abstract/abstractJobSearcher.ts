d6a61d8a621a172715e2fdd5307d06490ec97a91
KornelJanczak <korneljanczak10@gmail.com>
2024-10-19 15:24:05 +0200

    refactor(jobScraper): Update abstractJobSearcher.ts to remove unused imports and modify filter methods

--- apps/server/src/modules/jobScraper/services/abstract/abstractJobSearcher.ts
+++ apps/server/src/modules/jobScraper/services/abstract/abstractJobSearcher.ts
@@ -1,5 +1,4 @@
 import { type SearchJobOffers } from "../../interfaces";
-import { type Page } from "puppeteer";
 
 export abstract class AbstractJobSearcher {
   abstract searchJobOffers({
@@ -12,30 +11,25 @@ export abstract class AbstractJobSearcher {
   protected abstract filterLocation(
     path: string,
     location: string | undefined
-  ): string;
+  ): string | void;
   protected abstract filterTechStack(
     path: string,
     techStack: string[] | undefined
-  ): string;
+  ): string | void;
 
   protected abstract filterContent(
     path: string,
     content: string | undefined
-  ): string;
+  ): string | void;
 
   protected abstract filterPositionLevel(
     path: string,
     positionLevel: string | undefined
-  ): string;
+  ): string | void;
 
   protected abstract filterSalary(
     path: string,
     minimumSalary: number | undefined,
     maximumSalary: number | undefined
-  ): string;
-
-  // protected abstract filterJobType(
-  //   path: string,
-  //   jobType: string[] | undefined
-  // ): string;
+  ): string | void;
 }
