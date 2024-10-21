45f81013e8cbc35a8472af8e5d592253863b2b18
KornelJanczak <korneljanczak10@gmail.com>
2024-10-21 21:58:33 +0200

    refactor(jobScraper): Update dataCollector.ts to collect job offers from the posting list

--- apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/dataCollector.ts
+++ apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/dataCollector.ts
@@ -1,18 +1,46 @@
 import { Page } from "puppeteer";
 import { JobOffer } from "../../interfaces";
 import { AbstractDataCollector } from "../abstract/abstractDataCollector";
+import { autoScroll } from "../../helpers/autoScroll";
+import BadRequestError from "../../../../errors/badRequestError";
 
 class DataCollector extends AbstractDataCollector<JobOffer> {
   constructor() {
     super();
   }
 
-  async scrollAndCollectData(page: Page): Promise<JobOffer[]> {
-    throw new Error("Method not implemented.");
-  }
-
   async collectData(page: Page): Promise<JobOffer[]> {
-    throw new Error("Method not implemented.");
+    const elements = await page.$$(".posting-list-item");
+    const jobOffers: JobOffer[] = [];
+
+    for (const element of elements) {
+      try {
+        const [title, location, url] = await Promise.all([
+          element.$eval(
+            "h3.posting-title__position",
+            (el) => el.textContent?.trim() || ""
+          ),
+          element.$eval(
+            ".posting-info__location span",
+            (el) => el.textContent?.trim() || ""
+          ),
+          (await (await element.getProperty("href")).jsonValue()) as string,
+        ]);
+
+        jobOffers.push({ title, location, url });
+      } catch (err) {
+        throw new BadRequestError({
+          code: 400,
+          message: "Failed to collect data",
+          logging: true,
+          context: { error: err },
+        });
+      }
+    }
+
+    return jobOffers.map(
+      (job) => (job = { ...job, url: `https://nofluffjobs.com${job.url}` })
+    );
   }
 }
 
