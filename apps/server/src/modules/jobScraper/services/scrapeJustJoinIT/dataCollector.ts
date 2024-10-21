6338bcfb140f5f2c65420dddbb6c6eb0108c5e9b
KornelJanczak <korneljanczak10@gmail.com>
2024-10-21 21:58:43 +0200

    refactor(jobScraper): Simplify dataCollector.ts and remove unused code

--- apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/dataCollector.ts
+++ apps/server/src/modules/jobScraper/services/scrapeJustJoinIT/dataCollector.ts
@@ -4,10 +4,7 @@ import BadRequestError from "../../../../errors/badRequestError";
 import { autoScroll } from "../../helpers/autoScroll";
 import { AbstractDataCollector } from "../abstract/abstractDataCollector";
 
-class DataCollector
-  extends AbstractDataCollector<JustJoinITOffer>
-  implements IDataCollector<JustJoinITOffer>
-{
+class DataCollector extends AbstractDataCollector<JustJoinITOffer> {
   async collectData(page: Page): Promise<JustJoinITOffer[]> {
     const elements = await page.$$(".MuiBox-root.css-ai36e1");
     const jobOffers: JustJoinITOffer[] = [];
@@ -34,30 +31,6 @@ class DataCollector
     return jobOffers.map(
       (job) => (job = { ...job, url: `https://justjoin.it${job.url}` })
     );
-  }
-
-  async scrollAndCollectData(page: Page): Promise<JustJoinITOffer[]> {
-    const allJobs = new Set<JustJoinITOffer>();
-    let previousHeight = 0;
-    let currentHeight = 0;
-
-    while (true) {
-      const data = await this.collectData(page);
-      data.forEach((job) => allJobs.add(job));
-      await this.waitFor(3000);
-
-      await autoScroll(page);
-
-      currentHeight = await page.evaluate(() => document.body.scrollHeight);
-      const endOfPage = await page.$("footer");
-
-      if (endOfPage) break;
-      if (currentHeight === previousHeight) break;
-
-      previousHeight = currentHeight;
-    }
-
-    return Array.from(allJobs);
   }
 }
 
