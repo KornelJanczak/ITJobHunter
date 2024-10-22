af6ebd1b4566b2b8452c36499054ec161ba1d6ba
KornelJanczak <korneljanczak10@gmail.com>
2024-10-22 23:00:36 +0200

    refactor(jobScraper): Update services/index.ts to simplify and remove unused code

--- apps/server/src/modules/jobScraper/services/index.ts
+++ apps/server/src/modules/jobScraper/services/index.ts
@@ -1,48 +1,41 @@
 import { JobQuery } from "@repo/interfaces/job";
 import { ScrapeOptions } from "../interfaces";
 import { NextFunction } from "express";
-import { scrapeJustJoinIT } from "./scrapeJustJoinIT";
-import { scrapeNoFluffJobs } from "./scrapeNoFluffJobs";
+import NoFluffJobsScraperService from "./scrapeNoFluffJobs";
+import JobSearcher from "./scrapeNoFluffJobs/jobSearcher";
+// import { scrapeJustJoinIT } from "./scrapeJustJoinIT";
+// import { scrapeNoFluffJobs } from "./scrapeNoFluffJobs";
 
 interface IJobScraperService {
   scrapeJobs(jobQuery: JobQuery, next: NextFunction): Promise<any>;
 }
 
 class JobScraperService implements IJobScraperService {
-  urls = {
-    justJoinIT: "https://justjoin.it",
-    noFluffJobs: "https://nofluffjobs.com/pl",
-    protocolIT: "https://theprotocol.it/",
-    pracujPL: "https://www.pracuj.pl/",
-  };
-
   async scrapeJobs(jobQuery: JobQuery, next: NextFunction) {
     const jobs = await this.gatherJobs({
       jobQuery,
       next,
-      url: this.urls.justJoinIT,
     });
 
     return jobs;
   }
 
   private async gatherJobs(scrapeOptions: ScrapeOptions) {
-    const justJoinItJobs = await scrapeJustJoinIT.scrape({
-      ...scrapeOptions,
-      url: this.urls.justJoinIT,
-    });
+    // const justJoinItJobs = await scrapeJustJoinIT.scrape({
+    //   ...scrapeOptions,
+    // });
 
-    const noFluffJobs = await scrapeNoFluffJobs.scrape({
-      ...scrapeOptions,
-      url: this.urls.noFluffJobs,
-    });
 
-    const jobs = [...justJoinItJobs, ...noFluffJobs];
+
+    // const noFluffJobs = await scrapeNoFluffJobs.scrape({
+    //   ...scrapeOptions,
+    // });
+
+    const jobs = [];
 
     return jobs;
   }
 }
 
 const jobScraperService = new JobScraperService();
-
 export default jobScraperService;
