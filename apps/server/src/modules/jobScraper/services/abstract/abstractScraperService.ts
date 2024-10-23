5d05e0fbf031ee538fb5b97c3d6e218375dd778e
KornelJanczak <korneljanczak10@gmail.com>
2024-10-23 22:39:49 +0200

    refactor(jobScraper): Update AbstractScraperService to use constructor injection and simplify code

--- apps/server/src/modules/jobScraper/services/abstract/abstractScraperService.ts
+++ apps/server/src/modules/jobScraper/services/abstract/abstractScraperService.ts
@@ -1,64 +1,77 @@
-import puppeteer, { type Browser, type Page } from "puppeteer";
+import { type Page } from "puppeteer";
 import {
   IDataCollector,
   IJobSearcher,
   type ScrapeOptions,
 } from "../../interfaces";
 import BadRequestError from "../../../../errors/badRequestError";
+import JobSearcher from "../scrapeNoFluffJobs/jobSearcher";
+import DataCollector from "../scrapeNoFluffJobs/dataCollector";
+
+interface IScraperServiceDependencies<T> {
+  page: Page;
+  options: ScrapeOptions;
+  // jobSearcher: IJobSearcher;
+  // dataCollector: IDataCollector<T>;
+}
 
 export default abstract class AbstractScraperService<T> {
-  protected browser: Browser | null = null;
-  protected page: Page | null = null;
+  protected page: Page;
   protected options: ScrapeOptions;
-  protected jobSearcher: IJobSearcher;
-  protected dataCollector: IDataCollector<T>;
 
-  constructor(
-    options: ScrapeOptions,
-    jobSearcher: IJobSearcher,
-    dataCollector: IDataCollector<T>
-  ) {
+  // protected jobSearcher: IJobSearcher;
+  // protected dataCollector: IDataCollector<T>;
+
+  constructor({
+    page,
+    options,
+    // jobSearcher,
+    // dataCollector,
+  }: IScraperServiceDependencies<T>) {
     this.options = options;
-    this.jobSearcher = jobSearcher;
-    this.dataCollector = dataCollector;
+    this.page = page;
+    // this.jobSearcher = jobSearcher;
+    // this.dataCollector = dataCollector;
   }
 
-  async initBrowser() {
-    if (!this.browser) {
-      this.browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-      });
-    }
-  }
+  // async initBrowser() {
+  //   if (!this.browser) {
+  //     this.browser = await puppeteer.launch({
+  //       headless: false,
+  //       defaultViewport: null,
+  //     });
+  //   }
+  // }
 
-  async closeBrowser() {
-    if (this.browser) {
-      await this.browser.close();
-      this.browser = null;
-    }
-  }
+  // async closeBrowser() {
+  //   if (this.browser) {
+  //     await this.browser.close();
+  //     this.browser = null;
+  //   }
+  // }
 
-  async waitFor(time: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
+  // async waitFor(time: number): Promise<void> {
+  //   return new Promise((resolve) => setTimeout(resolve, time));
+  // }
+  abstract executeScrape(): Promise<T[]>;
 
   async scrape(): Promise<any> {
-    await this.initBrowser();
+    // await this.initBrowser();
 
-    if (!this.browser)
-      throw new BadRequestError({
-        message: "Browser is not initialized",
-        logging: true,
-        code: 400,
-      });
+    // if (!this.browser)
+    //   throw new BadRequestError({
+    //     message: "Browser is not initialized",
+    //     logging: true,
+    //     code: 400,
+    //   });
 
-    this.page = await this.browser.newPage();
+    // this.page = await this.browser.newPage();
 
     try {
       const result = await this.executeScrape();
-      await this.waitFor(100);
-      await this.closeBrowser();
+
+      // await this.waitFor(100);
+      // await this.closeBrowser();
 
       return result;
     } catch (err) {
@@ -69,10 +82,5 @@ export default abstract class AbstractScraperService<T> {
         context: { error: err },
       });
     }
-  }
-
-  private async executeScrape(): Promise<T[]> {
-    await this.jobSearcher.searchJobOffers();
-    return await this.dataCollector.scrollAndCollectData();
   }
 }
