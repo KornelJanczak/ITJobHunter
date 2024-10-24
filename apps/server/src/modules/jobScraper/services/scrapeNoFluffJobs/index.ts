500cb8e545eca7c7fd2b4a9a54127d03c21149df
KornelJanczak <korneljanczak10@gmail.com>
2024-10-24 22:26:43 +0200

    refactor(jobScraper): Refactor NoFluffJobsScraperService to use protected access modifiers and improve code structure

--- apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/index.ts
+++ apps/server/src/modules/jobScraper/services/scrapeNoFluffJobs/index.ts
@@ -1,55 +1,32 @@
-import { type Page } from "puppeteer";
-import { type IDataCollector } from "../../interfaces";
-import { type IJobSearcher } from "../../interfaces";
-import {
-  IJobScraperService,
-  IScraperServiceDependencies,
-  JobOffer,
-  ScrapeOptions,
-} from "../../interfaces";
+import { IJobScraperService, JobOffer } from "../../interfaces";
 import JobSearcher from "./jobSearcher";
 import AbstractScraperService from "../abstract/abstractScraperService";
 import DataCollector from "./dataCollector";
 
-class NoFluffJobsScraperService<T extends JobOffer>
-  extends AbstractScraperService<T>
-  implements IJobScraperService<T>
+class NoFluffJobsScraperService
+  extends AbstractScraperService
+  implements IJobScraperService<JobOffer>
 {
   private url: string = "https://nofluffjobs.com/pl/";
 
-  async executeScrape(): Promise<T[]> {
-    const jobSearcher = new JobSearcher(this.page, this.options);
-    const dataCollector = new DataCollector(this.page, "", this.url);
-
+  async executeScrape(): Promise<JobOffer[]> {
+    const { jobSearcher, dataCollector } = this.createScraperDependencies();
     await jobSearcher.searchJobOffers();
     const collectedData = await dataCollector.scrollAndCollectData();
 
-    // return collectedData;
+    return collectedData;
   }
-  // private jobSearcher: IJobSearcher;
-  // private dataCollector: IDataCollector<T>;
-  // constructor(
-  //   config: IScraperServiceDependencies<T>,
-  //   page: Page,
-  //   options: ScrapeOptions
-  // ) {
-  //   super(page, options);
-  //   this.jobSearcher = config.jobSearcher;
-  //   this.dataCollector = config.dataCollector;
-  // }
-  // protected async executeScrape(): Promise<any> {
-  //   // await this.jobSearcher.searchJobOffers({
-  //   //   page,
-  //   //   jobQuery,
-  //   //   next,
-  //   // });
-  //   // return await this.dataCollector.scrollAndCollectData(page);
-  // }
+
+  private createScraperDependencies() {
+    const jobSearcher = new JobSearcher(this.page, this.options);
+    const dataCollector = new DataCollector(
+      this.page,
+      ".posting-list-item",
+      this.url
+    );
+
+    return { jobSearcher, dataCollector };
+  }
 }
 
 export default NoFluffJobsScraperService;
-
-// export const scrapeNoFluffJobs = new NoFluffJobsScraperService({
-//   jobSearcher,
-//   dataCollector,
-// });
