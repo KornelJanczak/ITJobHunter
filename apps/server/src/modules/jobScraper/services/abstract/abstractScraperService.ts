e3e5b9000062423e89b2d6bba0006080b5f98461
KornelJanczak <korneljanczak10@gmail.com>
2024-10-24 22:26:32 +0200

    refactor(jobScraper): Remove unused import and fix type error in abstractScraperService.ts

--- apps/server/src/modules/jobScraper/services/abstract/abstractScraperService.ts
+++ apps/server/src/modules/jobScraper/services/abstract/abstractScraperService.ts
@@ -1,79 +1,26 @@
 import { type Page } from "puppeteer";
-import {
-  IDataCollector,
-  IJobSearcher,
-  type ScrapeOptions,
-} from "../../interfaces";
+import { type ScrapeOptions } from "../../interfaces";
 import BadRequestError from "../../../../errors/badRequestError";
-import JobSearcher from "../scrapeNoFluffJobs/jobSearcher";
-import DataCollector from "../scrapeNoFluffJobs/dataCollector";
 
-interface IScraperServiceDependencies<T> {
+interface IScraperServiceDependencies {
   page: Page;
   options: ScrapeOptions;
-  // jobSearcher: IJobSearcher;
-  // dataCollector: IDataCollector<T>;
 }
 
-export default abstract class AbstractScraperService<T> {
+export default abstract class AbstractScraperService {
   protected page: Page;
   protected options: ScrapeOptions;
 
-  // protected jobSearcher: IJobSearcher;
-  // protected dataCollector: IDataCollector<T>;
-
-  constructor({
-    page,
-    options,
-    // jobSearcher,
-    // dataCollector,
-  }: IScraperServiceDependencies<T>) {
+  constructor({ page, options }: IScraperServiceDependencies) {
     this.options = options;
     this.page = page;
-    // this.jobSearcher = jobSearcher;
-    // this.dataCollector = dataCollector;
   }
 
-  // async initBrowser() {
-  //   if (!this.browser) {
-  //     this.browser = await puppeteer.launch({
-  //       headless: false,
-  //       defaultViewport: null,
-  //     });
-  //   }
-  // }
-
-  // async closeBrowser() {
-  //   if (this.browser) {
-  //     await this.browser.close();
-  //     this.browser = null;
-  //   }
-  // }
-
-  // async waitFor(time: number): Promise<void> {
-  //   return new Promise((resolve) => setTimeout(resolve, time));
-  // }
-  abstract executeScrape(): Promise<T[]>;
+  abstract executeScrape(): Promise<any>;
 
   async scrape(): Promise<any> {
-    // await this.initBrowser();
-
-    // if (!this.browser)
-    //   throw new BadRequestError({
-    //     message: "Browser is not initialized",
-    //     logging: true,
-    //     code: 400,
-    //   });
-
-    // this.page = await this.browser.newPage();
-
     try {
-      const result = await this.executeScrape();
-
-      // await this.waitFor(100);
-      // await this.closeBrowser();
-
-      return result;
+      return await this.executeScrape();
     } catch (err) {
       throw new BadRequestError({
         message: "Failed to scrape",
