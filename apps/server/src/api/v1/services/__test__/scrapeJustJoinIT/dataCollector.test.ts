d8814c876b056f760898dd11b9fb95fadc81689d
KornelJanczak <korneljanczak10@gmail.com>
2024-10-29 20:20:17 +0100

    test: init test for dataCollector

--- apps/server/src/api/v1/services/__test__/scrapeJustJoinIT/dataCollector.test.ts
+++ apps/server/src/api/v1/services/__test__/scrapeJustJoinIT/dataCollector.test.ts
@@ -1,8 +1,22 @@
-import DataCollector from "../../scrapeJustJoinIT/dataCollector";
+import "expect-puppeteer";
+// import { beforeAll, describe, it } from "@jest/globals";
+import { Page } from "puppeteer";
 
-// describe("Test dataCollector", () => {
-//   beforeAll(async () => {
-//     const page = await page;
-//     const mockDataCollector = new DataCollector();
-//   });
-// });
+let timeout = 2500;
+
+describe("Test dataCollector", () => {
+  let page: Page;
+  beforeAll(async () => {
+    page = await global.__BROWSER__.newPage();
+    await page.goto("https://google.com");
+  }, timeout);
+
+  it('should be titled "Google"', async () => {
+    let text = await page.evaluate(() => document.body.textContent);
+    expect(text).toContain("abc");
+  });
+
+  afterAll(async () => {
+    await page.close();
+  });
+});
